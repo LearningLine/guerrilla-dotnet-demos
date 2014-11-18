9d095444964f332b02581f221c0c08ce2c7407d0
Michael Kennedy <mikeckennedy@gmail.com>
2014-11-18 19:32:41 +0000

    starter code for mocking / test doubles.

---
+++ 12_Mocking/web.Test/HomeControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using ef_data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using web.Controllers;
+
+namespace web.Test
+{
+	[TestClass]
+	public class HomeControllerTests
+	{
+		[TestMethod]
+		public void Index_WillReturnOnlyFiveGenres()
+		{
+			var sut = new HomeController(new TestUnitOfWork());
+
+			ViewResult view = sut.Index();
+
+			var genres = (IEnumerable<Genre>) view.Model;
+
+			Assert.AreEqual(5, genres.Count());
+		}
+	}
+}
