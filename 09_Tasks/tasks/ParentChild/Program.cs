dc3835aeb21617ff68e9db1b02481e36a566988c
GNETUK <salesuk@develop.com>
2014-11-19 10:50:31 +0000

    first half of threading / tasks.

---
+++ 09_Tasks/tasks/ParentChild/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ParentChild
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            string[] urls =
+            {
+                "http://",
+                "http://",
+                "http://",
+                "http://",
+                "http://"
+            };
+        }
+    }
+}
