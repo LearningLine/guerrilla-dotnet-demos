b1ebd51992fec343f37c198de549d7f5209116b9
GNETUK <salesuk@develop.com>
2014-11-19 19:55:53 +0000

    with async mvc and entity framework starter.

---
+++ 11_Async/MvcAsync/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+
+namespace MvcAsync.Controllers
+{
+    public class HomeController : Controller
+    {
+        ChinookEntities entities = new ChinookEntities();
+        public ActionResult Index()
+        {
+            List<author> authors =
+                (from a in entities.authors
+                where a.state == "CA"
+                orderby a.au_lname
+                select a)
+                .Take(10)
+                .ToList();
+
+            return View(authors);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
