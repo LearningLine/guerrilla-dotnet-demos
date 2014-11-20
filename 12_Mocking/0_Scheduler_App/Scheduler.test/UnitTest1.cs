ee5880c070ecba240afdad80d53a0004027bbe5c
GNETUK <salesuk@develop.com>
2014-11-20 11:17:01 +0000

    Moq

--- 12_Mocking/0_Scheduler_App/Scheduler.test/UnitTest1.cs
+++ 12_Mocking/0_Scheduler_App/Scheduler.test/UnitTest1.cs
@@ -1,23 +1,58 @@
 ﻿using System;
 using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using TheScheduler;
 
 namespace Scheduler.test
 {
     [TestClass]
-    public class SchedulingServiceTests
+    public class SchedulingServiceTest
     {
+        private Mock<ILogger> logger;
+        private Mock<ITimeService> timeService;
+
+        private SchedulingService CreateSut()
+        {
+            return new SchedulingService(logger.Object, timeService.Object);
+        }
+
+        [TestInitialize]
+        public void Init()
+        {
+            logger = new Mock<ILogger>();
+            timeService = new Mock<ITimeService>();
+        }
+
         [TestMethod]
         public void AScheduledJob_ShouldCompleteOnTime()
         {
-            var sut = new SchedulingService(null, null);
+            timeService.SetupSequence(ts => ts.Now)
+                       .Returns(DateTime.Now)
+                       .Returns(DateTime.Now +
+                                TimeSpan.FromDays(300) +
+                                TimeSpan.FromSeconds(1));
+
+            var sut = CreateSut();
 
             sut.AddJob(1, "Hello", TimeSpan.FromDays(300));
 
-            Thread.Sleep(1200);
+            //timeService.Setup(ts => ts.Now)
+            //           .Returns(DateTime.Now + 
+            //                    TimeSpan.FromDays(300) + 
+            //                    TimeSpan.FromSeconds(1));
 
             Assert.IsTrue(sut.IsJobFinished(1));
+        }
+
+        [TestMethod]
+        public void AScheduledJob_ShouldCauseALogRecord()
+        {
+            var sut = CreateSut();
+
+            sut.AddJob(1, "Hello", TimeSpan.FromDays(300));
+
+            logger.Verify(l => l.Message(It.IsAny<string>()), Times.AtLeastOnce);            
         }
     }
 
--- 12_Mocking/web.Test/HomeControllerTests.cs
+++ 12_Mocking/web.Test/HomeControllerTests.cs
@@ -2,7 +2,9 @@
 using System.Linq;
 using System.Web.Mvc;
 using ef_data;
+using Interfaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using web.Controllers;
 
 namespace web.Test
@@ -13,7 +15,30 @@ namespace web.Test
 		[TestMethod]
 		public void Index_WillReturnOnlyFiveGenres()
 		{
-			var sut = new HomeController(new TestUnitOfWork());
+            var uow = new Mock<IUnitOfWork>(); 
+
+            var repo = new Mock<IGenreRepository>();
+
+		    uow.Setup(u => u.Genres)
+		        .Returns(repo.Object);
+
+            repo.Setup(r => r.Items)
+                .Returns(new List<Genre>
+				    {
+					    new Genre {Name = "Test"},
+					    new Genre {Name = "Test"},
+					    new Genre {Name = "Test"},
+					    new Genre {Name = "Test"},
+					    new Genre {Name = "Test"},
+					    new Genre {Name = "Test"},
+					    new Genre {Name = "Test"},
+					    new Genre {Name = "Test"},
+					    new Genre {Name = "Test"},
+					    new Genre {Name = "Test"},
+				    }.AsQueryable());
+
+
+			var sut = new HomeController(uow.Object);
 
 			ViewResult view = sut.Index();
 
