0af14b29ba1653751fd0a2539765837423e3e08d
gnetuk <gnetuk@michaelckennedy.net>
2015-03-30 15:32:43 -0400

    added ef demos.

---
+++ 01_LINQ_and_EF/EfDemo/EfDemo/Migrations/Configuration.cs
@@ -0,0 +1,33 @@
+namespace EfDemo.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EfDemo.PeopleContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "EfDemo.PeopleContext";
+        }
+
+        protected override void Seed(EfDemo.PeopleContext context)
+        {
+            
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
