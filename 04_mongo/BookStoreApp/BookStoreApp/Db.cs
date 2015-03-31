b8f9fa49625f86feefd34bdf709705fdc746f478
Michael Kennedy <mikeckennedy@gmail.com>
2015-03-30 20:48:04 -0400

    mongo demos.

---
+++ 04_mongo/BookStoreApp/BookStoreApp/Db.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BookStoreApp
+{
+	class Db : MongoDB.Kennedy.MongoDbDataContext
+	{
+		public IQueryable<Book> Books { get { return base.GetCollection<Book>(); } }
+
+
+		public Db() : base("GNETBooks")
+		{
+		}
+	}
+
+	//[BsonIgnoreExtraElements]
+	class Book
+	{
+		public ObjectId Id { get; set; }
+
+		public string Title { get; set; }
+		public int PageCount { get; set; }
+		public bool IsPrint { get; set; }
+
+		public List<Review> Reviews { get; set; }
+
+		[BsonExtraElements]
+		public BsonDocument AddtionalData { get; set; }
+
+		public Book()
+		{
+			IsPrint = true;
+			Reviews = new List<Review>();
+		}
+	}
+
+	internal class Review
+	{
+		public DateTime Date { get; set; }
+		public string Comment { get; set; }
+		public int Stars { get; set; }
+		public string UserName { get; set; }
+
+		public Review()
+		{
+			Date = DateTime.UtcNow;
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 04_mongo/BookStoreApp/BookStoreApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB;
+
+namespace BookStoreApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//CreateData();
+			QueryData();
+		}
+
+		private static void QueryData()
+		{
+			var db = new Db();
+
+			var longBooks =
+				from b in db.Books
+				where b.PageCount >= 500
+				orderby b.PageCount descending
+				select b;
+
+			Console.WriteLine("The query is:");
+			Console.WriteLine(longBooks.ToMongoQueryText());
+
+			foreach (var b in longBooks)
+			{
+				Console.WriteLine("{0} with {1:N0} pages", b.Title, b.PageCount);
+			}
+
+		}
+
+		private static void CreateData()
+		{
+			var db = new Db();
+
+
+			Book b = new Book();
+			b.Title = "Fancy db programming v1";
+			b.PageCount = 201;
+			b.Reviews.Add(new Review() {Comment = "Loved it!", Stars = 5});
+			b.Reviews.Add(new Review() {Comment = "Meh", Stars = 3});
+
+			db.Save(b);
+
+			 b = new Book();
+			b.Title = "LOLCat programming callenges";
+			b.PageCount = 1001;
+
+			db.Save(b);
+		}
+	}
+}
