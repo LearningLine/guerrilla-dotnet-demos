db228d11e379efbca46eeb75fdc5184e1e7e2087
gnetuk <gnetuk@michaelckennedy.net>
2015-04-01 11:33:27 -0400

    MVC and Katana security

---
+++ 09_MVCSecurity/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // User.Identity.IsAuthenticated
+            // Request.IsAuthenticated
+            // User.Identity.Name
+            // User.IsInRole("Admin")
+
+            if (User.Identity.IsAuthenticated == false)
+            {
+
+            }
+
+            var user = (ClaimsPrincipal)User;
+
+            
+            var emailClaim = user.Claims
+                .Where(x => x.Type == "email")
+                .FirstOrDefault();
+            
+            if (emailClaim != null)
+            {
+                var email = emailClaim.Value;
+            }
+                
+            //User.IsInRole()
+
+            return View();
+        }
+
+        [Authorize(Roles="Manager")]
+        public ActionResult Secure()
+        {
+            //User.IsInRole()
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Login(string username, string returnUrl)
+        {
+            if (username == "brock")
+            {
+                // Rfc2898DeriveBytes
+                // MembershipReboot
+                var claims = new Claim[]{
+                    new Claim("name", "Brock"),
+                    new Claim("email", "brockallen@gmail.com"),
+                    new Claim("id", "123"),
+                    new Claim("role", "Admin"),
+                    new Claim("role", "Dev"),
+                    new Claim("status", "Gold"),
+                };
+                
+                var ci = new ClaimsIdentity(
+                    claims,
+                    "Cookies", 
+                    "name", "role");
+
+                var ctx = Request.GetOwinContext();
+                ctx.Authentication.SignIn(ci);
+
+                if (Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return Redirect("~/Home");
+                }
+            }
+
+            ModelState.AddModelError("", "Invalid username or password");
+            return View("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            var ctx = Request.GetOwinContext();
+            ctx.Authentication.SignOut("Cookies");
+            return Redirect("~/");
+        }
+    }
+}
---
+++ 09_MVCSecurity/WebApplication1/WebApplication1/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(
+                new CookieAuthenticationOptions
+                {
+                    AuthenticationType = "Cookies",
+                    CookieHttpOnly = true,
+                    CookieSecure = CookieSecureOption.SameAsRequest,
+                    SlidingExpiration = true,
+                    ExpireTimeSpan = TimeSpan.FromHours(1),
+                    //LoginPath = new PathString("/Home/Login")
+                });
+
+            app.Use(async (ctx, next) =>
+            {
+                if (ctx.Authentication.User != null &&
+                    ctx.Authentication.User.Identity.IsAuthenticated)
+                {
+                    var ci = (ClaimsIdentity)ctx.Authentication.User.Identity;
+                    ci.AddClaim(new Claim("custom", "from DB"));
+                }
+
+                await next();
+                //if (ctx.Response.StatusCode == 401)
+                //{
+                //    if (ctx.Authentication.User.Identity.IsAuthenticated)
+                //    {
+                //        ctx.Response.Redirect("/Home/Forbidden");
+                //    }
+                //    else
+                //    {
+                //        ctx.Response.Redirect("/Home/Login");
+                //    }
+                //}
+            });
+        }
+
+    }
+}
