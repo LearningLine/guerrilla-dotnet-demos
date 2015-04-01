594a04dd379ce6ed4f9854e210966acaf8e98a0c
gnetuk <gnetuk@michaelckennedy.net>
2015-04-01 12:35:32 -0400

    SSO and OIDC

---
+++ 09_MVCSecurity/SSO/SSO/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SSO.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Login()
+        {
+            return Redirect("~/");
+        }
+
+        public ActionResult Logout()
+        {
+            Request.GetOwinContext().Authentication.SignOut("Cookies");
+            return Redirect("~/");
+        }
+    }
+}
---
+++ 09_MVCSecurity/SSO/SSO/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+
+namespace SSO
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            JwtSecurityTokenHandler.InboundClaimTypeMap = new Dictionary<string, string>();
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "Cookies"
+            });
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            {
+                Authority = "https://localhost:44333/core",
+                ClientId = "implicitclient",
+                RedirectUri = "http://localhost:12209/",
+                ResponseType = "id_token",
+                Scope = "openid email profile",
+                SignInAsAuthenticationType = "Cookies",
+                UseTokenLifetime = false
+            });
+        }
+    }
+}
