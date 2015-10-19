834f4737385ae5900663153c40bcd8184f97f046
Scott Reed <scott@brainhzsoftware.com>
2015-10-19 14:34:48 -0700

    2nd lab

---
+++ 02_EntityFramework/EF/Polling.Entities/Choice.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Polling.Entities
+{
+    public class Choice
+    {
+        public int Id { get; set; }
+        public Poll Poll { get; set; }
+        public int PollId { get; set; }
+        public ICollection<Vote> Votes { get; set; }
+        [StringLength(100)]
+        public string ChoiceText { get; set; }
+    }
+}
---
+++ 02_EntityFramework/EF/Polling.Entities/Vote.cs
@@ -0,0 +1,9 @@
+﻿namespace Polling.Entities
+{
+    public class Vote
+    {
+        public int Id { get; set; }
+        public Choice Choice { get; set; }
+        public int ChoiceId { get; set; }
+    }
+}
