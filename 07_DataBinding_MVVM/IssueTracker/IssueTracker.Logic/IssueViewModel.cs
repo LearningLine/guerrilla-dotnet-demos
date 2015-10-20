e37a17cd69af2f7f4add9f4de5defbd99990a6bc
John Bowen <john@bowencode.net>
2015-10-20 14:35:14 -0700

    Updating start code

---
+++ 07_DataBinding_MVVM/IssueTracker/IssueTracker.Logic/IssueViewModel.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using IssueTracker.Data;
+
+namespace IssueTracker.Logic
+{
+    public class IssueViewModel : Bindable
+    {
+        private Issue _issue;
+
+        public IssueViewModel(Issue word)
+        {
+            _issue = word;
+        }
+
+        public Issue Issue { get { return _issue; } }
+
+        public string Text
+        {
+            get { return _issue.Text; }
+        }
+
+        public int Count
+        {
+            get { return _issue.ReportCount; }
+        }
+
+        private DateTime _lastChange;
+        public DateTime LastChange
+        {
+            get { return _lastChange; }
+            private set
+            {
+                if (_lastChange == value)
+                    return;
+                _lastChange = value;
+                //OnPropertyChanged();
+            }
+        }
+
+        public void Update(DateTime time)
+        {
+            LastChange = time;
+            //OnPropertyChanged(() => Count);
+        }
+    }
+}
---
+++ 07_DataBinding_MVVM/IssueTracker/IssueTracker.Logic/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IssueTracker.Data;
+using Microsoft.Practices.Prism.Commands;
+
+namespace IssueTracker.Logic
+{
+    public class MainViewModel
+    {
+        private ObservableCollection<IssueViewModel> _issues = new ObservableCollection<IssueViewModel>();
+        private IIssueService _service;
+
+        public MainViewModel()
+        {
+            _service = null;
+        }
+
+        public User User { get; private set; }
+
+
+        private bool CanLogin()
+        {
+            return User == null;
+        }
+
+        private void Login(string username)
+        {
+            User = new User { Name = username };
+        }
+
+        private void EnterIssue()
+        {
+            string issueText = "";
+
+            DateTime reportTime = DateTime.Now;
+            Issue issue = _service.GetIssue(issueText);
+            if (issue == null)
+            {
+                issue = new Issue { Text = issueText, Users = new List<User>() };
+            }
+            _service.ReportIssue(issue, User);
+
+
+        }
+    }
+}
