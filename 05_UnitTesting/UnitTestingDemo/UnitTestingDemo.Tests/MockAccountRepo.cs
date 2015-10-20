626ad0db08ad26cc9234813f3ab537f25b98e75b
Brad Cunningham <Brad@BradCunningham.net>
2015-10-20 11:46:39 -0700

    Unit testing demo

---
+++ 05_UnitTesting/UnitTestingDemo/UnitTestingDemo.Tests/MockAccountRepo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingDemo.Tests
+{
+	public class MockAccountRepository : IAccountRepository
+	{
+		private decimal balance;
+
+		public MockAccountRepository(int id)
+		{
+			balance = 15.0m;
+		}
+
+		public decimal Deposit(decimal amount)
+		{
+			balance += amount;
+			return balance;
+		}
+
+		public decimal Withdraw(decimal amount)
+		{
+			balance -= amount;
+			return balance;
+		}
+
+		public decimal GetBalance()
+		{
+			return balance;
+		}
+	}
+}
