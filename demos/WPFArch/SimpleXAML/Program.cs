b494591d5baa683b136350a525b9045207186963
Instructors <gnetuk@michaelckennedy.net>
2015-11-23 17:58:19 +0000

    final wpf

---
+++ demos/WPFArch/SimpleXAML/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Markup;
+
+namespace SimpleXAML
+{
+
+    public class Person : DependencyObject
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} is {1}", this.Name, this.Age);
+        }
+    }
+
+    [ContentProperty("Employees")]
+    public class Company
+    {
+        public string Name { get; set; }
+        public Person Owner { get; set; }
+
+        public List<Person> Employees { get; set; }
+
+        public static DependencyProperty EmployeeIdProperty =
+            DependencyProperty.RegisterAttached("EmployeeId",
+                typeof(int), typeof(Company));
+
+        public static void SetEmployeeId(Person p, int id)
+        {
+           p.SetValue(Company.EmployeeIdProperty, id);
+        }
+        public static int GetEmployeeId(Person p)
+        {
+            return (int) p.GetValue(Company.EmployeeIdProperty);
+        }
+
+        public Company()
+        {
+            Employees = new List<Person>();
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Name: {0}, Owner: {1}", 
+                Name, Owner);
+        }
+    }
+
+    public class RandomValue : MarkupExtension
+    {
+        private static Random rnd = new Random();
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return rnd.Next(Min, Max);
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //using (var file = File.OpenRead(@"..\..\person.xml"))
+            //{
+            //    Person p = (Person) XamlReader.Load(file);
+            //    Console.WriteLine(p);
+            //}
+
+            using (var file = File.OpenRead(@"..\..\company.xml"))
+            {
+                Company c = (Company)XamlReader.Load(file);
+                Console.WriteLine(c);
+                foreach (var e in c.Employees)
+                {
+                    Console.WriteLine("  {0}  {1}  ", 
+                        Company.GetEmployeeId(e), e);
+                }
+            }
+        }
+    }
+}
---
+++ demos/WPFArch/SurfaceLite/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using PictureLibrary;
+
+namespace SurfaceLite
+{
+    public partial class MainWindow : Window
+    {
+        private WebPageImageExtractor extractor = new WebPageImageExtractor();
+        private Random rnd = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            extractor.NewImage += Extractor_NewImage;
+        }
+
+        private void Extractor_NewImage(object sender, ImageEventArgs e)
+        {
+            BitmapImage imgSrc = new BitmapImage(e.Uri);
+            Image img = new Image();
+            img.Source = imgSrc;
+            img.Width = 200;
+            Canvas.SetLeft(img, rnd.NextDouble()*surface.ActualWidth);
+            Canvas.SetTop(img, rnd.NextDouble()*surface.ActualHeight);
+            img.RenderTransform = new RotateTransform(rnd.Next(-20,20));
+
+            surface.Children.Add(img);
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+
+            string uri = this.uriTextBox.Text;
+            extractor.FindImagesAsync(new Uri(uri));
+        }
+    }
+}
