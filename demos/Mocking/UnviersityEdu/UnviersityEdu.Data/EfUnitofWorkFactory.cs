d9616759aabc710bf97d9db2c283d11944a1e634
Michael Kennedy <mikeckennedy@gmail.com>
2015-11-25 10:40:36 +0000

    starter code for mocking demos.

---
+++ demos/Mocking/UnviersityEdu/UnviersityEdu.Data/EfUnitofWorkFactory.cs
@@ -0,0 +1,12 @@
+﻿using UnviersityEdu.Objects;
+
+namespace UnviersityEdu.Data
+{
+    public class EfUnitofWorkFactory : IUnitOfWorkFactory
+    {
+        public IUnitOfWork Create()
+        {
+            return new SchoolContext();
+        }
+    }
+}
---
+++ demos/Mocking/UnviersityEdu/UnviersityEdu.Data/SchoolContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using UnviersityEdu.Objects;
+
+namespace UnviersityEdu.Data
+{
+    public class SchoolContext : DbContext, IUnitOfWork
+	{
+		public SchoolContext() : base("UniversityEdu")
+		{
+            CourseRepository = new EfCourseRepository(this);
+		}
+
+		public DbSet<Student> Students { get; set; }
+		public DbSet<Enrollment> Enrollments { get; set; }
+		public DbSet<Course> Courses { get; set; }
+
+        public ICourseRepository CourseRepository { get; set; }
+        public void Commit()
+        {
+            SaveChanges();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+		}
+	}
+}
