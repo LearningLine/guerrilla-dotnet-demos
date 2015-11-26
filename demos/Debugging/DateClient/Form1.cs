dc9a1d82a393dca7c97aca5cb7aea1788620afc2
Michael Kennedy <mikeckennedy+github@gmail.com>
2015-11-26 10:58:05 +0000

    debugging start.

---
+++ demos/Debugging/DateClient/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ServiceModel;
+
+namespace DateClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+				label2.Text = TryLine(textBox1.Text);
+
+	        bool success = label2.Text.ToLower().Contains("[success]");
+			label2.BackColor = success ? Color.LightGreen : Color.LightPink;
+        }
+    }
+}
